382fd8b8d2132cf78650ca6b969c5b728a7bbd69
Rhcandy <helenacandy0@gmail.com>
2025-05-31 18:10:59 +0300

    azert

--- demo/src/main/java/rhjava/erpnext/demo/model/SalarySlip.java
+++ demo/src/main/java/rhjava/erpnext/demo/model/SalarySlip.java
@@ -1,8 +1,11 @@
 package rhjava.erpnext.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SalarySlip {
     String name;
     String status;
